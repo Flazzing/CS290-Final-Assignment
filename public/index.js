4c230a1c79e98704691feacf493922f390d26647
brownit89 <35484067+brownit89@users.noreply.github.com>
2018-06-09 22:47:23 -0700

    filled in some js/updated html
    need modals to finish js, but outline is there similar to project 5s.  Updated "creators" to "users" and "clyps" to "clips"

--- public/index.js
+++ public/index.js
@@ -1,1 +1,33 @@
-<!--Placeholder-->
+var allUsers = [];
+var allClips = [];
+
+function handleModalUploadClick() {
+
+  var clipName = document.getElementById('name-input').value;  //element located in modal
+  var clipText = document.getElementById('clip-content').value;
+
+  if(clipName && clipText) {
+    allClips.push({
+      text: clipText,
+      author: clipName
+    });
+  } else{
+    alert('You must specify both a username and a comment!');
+  }
+}
+
+//finish once modals are uploaded
+function showUploadClipModal(){
+
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var createClipModal = document.getElementById('create-clip-modal');
+
+  modalBackdrop.classList.remove('hidden');
+  createTwitModal.classList.remove('hidden');
+}
+
+function clearClipInputValues(){
+}
+
+function hideCreateClipModal(){
+}
