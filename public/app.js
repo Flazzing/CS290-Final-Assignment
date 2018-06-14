17228b509627fa7cb49b3693d094894152842da7
brownit89 <35484067+brownit89@users.noreply.github.com>
2018-06-14 15:18:21 -0700

    recording function working

--- public/app.js
+++ public/app.js
@@ -3,15 +3,25 @@ navigator.getUserMedia = ( navigator.getUserMedia ||
                        navigator.mozGetUserMedia ||
                        navigator.msGetUserMedia);
 
+var record = document.querySelector('.record');
+var stop = document.querySelector('.stop');
+var soundClips = document.querySelector('.sound-clips');
+var canvas = document.querySelector('.visualizer');
+
+var audioCtx = new (window.AudioContext || webkitAudioContext)();
+var canvasCtx = canvas.getContext("2d");
+
 if(navigator.getUserMedia){
   console.log('getUserMedia supported.');
-  navigator.mediaDevices.getUserMedia(
+  navigator.getUserMedia(
     {
     audio:true
-  })
+  },
 
-  .then(function(stream){
+  function(stream){
     var mediaRecorder = new MediaRecorder(stream);
+
+    visualize(stream);
 
     record.onclick = function(){
       mediaRecorder.start();
@@ -21,12 +31,107 @@ if(navigator.getUserMedia){
       record.style.color = 'black';
     }
 
-    var clipStream = [];
-
     mediaRecorder.ondataavailable = function(e){
-      clipStream.push(e.data);
-    }
+      console.log('data available');
 
+      var clipName = prompt('enter a name for clip');
+      var clipContainer = document.createElement('article');
+      var clipLabel = document.createElement('p');
+      var audio = document.createElement('audio');
+      var deleteButton = document.createElement('button');
+
+      clipContainer.classList.add('clip');
+      audio.setAttribute('controls', '');
+      deleteButton.innerHTML = 'Delete';
+      clipLabel.innerHTML = clipName;
+
+      clipContainer.appendChild(audio);
+      clipContainer.appendChild(clipLabel);
+      clipContainer.appendChild(deleteButton);
+      soundClips.appendChild(clipContainer);
+
+      var audioURL = window.URL.createObjectURL(e.data);
+      audio.src = audioURL;
+
+      deleteButton.onclick = function(e) {
+        var eventTarget = e.target;
+        eventTarget.parentNode.parentNode.removeChild(eventTarget.parentNode);
+    }
+  }
+
+  stop.onclick = function() {
+  mediaRecorder.stop();
+  console.log(mediaRecorder.state);
+  console.log("recorder stopped");
+  record.style.background = "";
+  record.style.color = "";
+}
+},
+
+
+  function(err) {
+     console.log('The following gUM error occured: ' + err);
+  }
+
+);
+} else {
+console.log('getUserMedia not supported on your browser!');
+}
+
+function visualize(stream) {
+var source = audioCtx.createMediaStreamSource(stream);
+
+var analyser = audioCtx.createAnalyser();
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+
+source.connect(analyser);
+//analyser.connect(audioCtx.destination);
+
+WIDTH = canvas.width
+HEIGHT = canvas.height;
+
+draw()
+
+function draw() {
+
+requestAnimationFrame(draw);
+
+analyser.getByteTimeDomainData(dataArray);
+
+canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+canvasCtx.lineWidth = 2;
+canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+canvasCtx.beginPath();
+
+var sliceWidth = WIDTH * 1.0 / bufferLength;
+var x = 0;
+
+
+for(var i = 0; i < bufferLength; i++) {
+
+var v = dataArray[i] / 128.0;
+var y = v * HEIGHT/2;
+
+if(i === 0) {
+  canvasCtx.moveTo(x, y);
+} else {
+  canvasCtx.lineTo(x, y);
+}
+
+x += sliceWidth;
+}
+
+canvasCtx.lineTo(canvas.width, canvas.height/2);
+canvasCtx.stroke();
+
+}
+}
+/*
     stop.onclick = function(){
       mediaRecorder.stop();
       console.log(mediaRecorder.state);
@@ -36,36 +141,12 @@ if(navigator.getUserMedia){
     }
     mediaRecorder.onstop=function(e){ //On stop, recording should load into player
       console.log('recorder stopped');
-      var clipsFullContainer = document.querySelector('clips-content');
-      var clipName = document.querySelector('input_text');
-      var clipContainer = document.createElement('article');
-      var clipLabel = document.createElement('p');
-      var audio = document.createElement('audio');
-      var deleteButton = document.createElement('button');
 
-      clipContainer.classList.add('clip');
-      audio.setAttribute('controls', '');
-      deleteButton.innerHTML = "Delete";
-      clipLabel.innerHTML = clipName;
-
-      clipContainer.appendChild(audio);
-      clipContainer.appendChild(clipLabel);
-      clipContainer.appendChild(deleteButton);
-      clipsFullContainer.appendChild(clipContainer);
-
-      var blob = new Blob(clipStream, {'type':'audio/ogg; codecs=opus'});
-      clipStream= [];
-      var audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-
-      deleteButton.onclick = function(e) {
-        var eventTarget = e.target;
-        eventTarget.parentNode.parentNode.removeChild(eventTarget.parentNode);
 
       }
 
     }
-  })
+  }
 
   .catch(function(err){
     console.log('The following getUserMedia error occurred: ' + err);
@@ -74,3 +155,4 @@ if(navigator.getUserMedia){
 } else{
   console.log('getUserMedia not supported on your browser!');
 }
+*/
