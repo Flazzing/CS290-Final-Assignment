cedccf08fe609481acd825fa296a9d99b832e8ff
isvpursuit <34354663+isvpursuit@users.noreply.github.com>
2018-06-14 07:07:46 -0700

    Index.js Housekeeping
    Rewrote some of the variables so that they better describe their actual intended use, added comments to functions, cleaned up some of the text for readability

--- public/index.js
+++ public/index.js
@@ -1,11 +1,54 @@
 var record = document.querySelector('.record');
 var stop = document.querySelector('.stop');
 var clipContainer = document.querySelector('.clips-full-container');
-var inputText = document.getElementById('input_text');
-var contributorText = document.getElementById('contributor_text')
+var authorText = document.getElementById('author_text');
+var commentText = document.getElementById('comment_text')
 
+/////////////////////////////////
+//  Display or hide the modal  //
+/////////////////////////////////
+function display_modal(){
+	var modal = document.getElementById('modal_container');
+	var modal_backdrop = document.getElementById('modal_backdrop');
+	modal.classList.remove('hide_modal');
+	modal_backdrop.classList.remove('hide_modal');
+  clearTextFields();
+};
+
+function hide_modal(){
+	var modal = document.getElementById('modal_container');
+	var modal_backdrop = document.getElementById('modal_backdrop');
+	modal.classList.add('hide_modal');
+	modal_backdrop.classList.add('hide_modal');
+};
+
+//////////////////////////////////////////////////////
+//  Clears the Author and Comments fields in modal  //
+//////////////////////////////////////////////////////
+function clearTextFields(){
+  authorText.value = '';
+  commentText.value = '';
+};
+
+///////////////////////////////////
+//  Conditional check on upload  //
+///////////////////////////////////
+function handleModalUploadClick() {
+  var clipAuthor = authorText.value;
+  var clipComment = commentText.value;
+  if(clipAuthor && clipComment) {
+  	insertNewClip(clipAuthor, clipComment);
+  	hide_modal();
+  } else{
+    alert('You must specify both a username and a comment!');
+  }
+};
+
+/////////////////////////////////////////
+//  Inserts the new clip into the DOM  //
+//    input: string, string            //
+/////////////////////////////////////////
 function insertNewClip(clipAuthor, clipComments) {
-
   var clipTemplate = Handlebars.templates.audioclip;
   var clipHTML = clipTemplate({
     author: clipAuthor,
@@ -14,34 +57,18 @@ function insertNewClip(clipAuthor, clipComments) {
   clipContainer.insertAdjacentHTML('beforeend', clipHTML);
 };
 
-function clearTextFields(){
-  inputText.value = '';
-  contributorText.value = '';
-};
-
-function handleModalUploadClick() {
-
-  var clipName = document.getElementById('name-input').value;  //element located in modal
-  var clipText = document.getElementById('clip-content').value;
-
-  if(clipName && clipText) {
-    allClips.push({
-      text: clipText,
-      author: clipName
-    });
-  } else{
-    alert('You must specify both a username and a comment!');
-  }
-};
-
-
+///////////
+//  ???  //
+///////////
 function recordClip(){
   record.addEventListener('click', function(){
     getUserMedia();
   });
 };
 
-/* Display modal when Upload clicked */
+//////////////
+//  Events  //
+//////////////
 var upload = document.getElementById('upload-button');
 upload.addEventListener('click', function() {
 	display_modal();
@@ -59,23 +86,7 @@ cancel_button.addEventListener('click', function() {
 
 var submit_button = document.getElementsByClassName('modal_submit')[0];
 submit_button.addEventListener('click', function() {
-	insertNewClip( inputText.value, contributorText.value);
+	handleModalUploadClick();
+	clearTextFields();
 	hide_modal();
-  clearTextFields();
 });
-
-
-function display_modal(){
-	var modal = document.getElementById('modal_container');
-	var modal_backdrop = document.getElementById('modal_backdrop');
-	modal.classList.remove('hide_modal');
-	modal_backdrop.classList.remove('hide_modal');
-  clearTextFields();
-};
-
-function hide_modal(){
-	var modal = document.getElementById('modal_container');
-	var modal_backdrop = document.getElementById('modal_backdrop');
-	modal.classList.add('hide_modal');
-	modal_backdrop.classList.add('hide_modal');
-};
