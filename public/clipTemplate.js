927391ca788a0f164d94c35d34201b47d0a9c03c
isvpursuit <34354663+isvpursuit@users.noreply.github.com>
2018-06-13 19:31:16 -0700

    Housekeeping
    Tidied up some of the code a bit for readability.

--- public/clipTemplate.js
+++ public/clipTemplate.js
@@ -3,10 +3,10 @@
 templates['audioclip'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<main class=\"clips-container\">\r\n    <div class=\"clips-icon\">\r\n        <i class=\"profile\">\r\n            <i><img src = \"Logo.png\" height=\"55\" width=\"65\"></i>\r\n            <i>\r\n              "
+  return "<article class=\"clips-container\">\r\n    <div class=\"clips-icon\">\r\n        <i class=\"profile\">\r\n            <i><img src = \"Logo.png\" height=\"55\" width=\"65\"></i>\r\n            <i>\r\n\r\n              "
     + alias4(((helper = (helper = helpers.author || (depth0 != null ? depth0.author : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"author","hash":{},"data":data}) : helper)))
-    + "\r\n            </i>\r\n        </i>\r\n    </div>\r\n    <article class=\"clips\">\r\n        <button type=\"button\" class = \"play-button\">\r\n            <img src= \"playbutton.png\" height=\"42\" width=\"42\">\r\n        </button>\r\n\r\n        <div class=\"clips-content\">\r\n            <p class=\"clips-text\">API SHOULD BE EXTEND FROM HERE\r\n                <p1 class=\"clips-time\">3:58</p1>\r\n            </p>\r\n        </div>\r\n    </article>\r\n    <div class = \"author-comments\">\r\n\r\n      "
+    + "\r\n\r\n            </i>\r\n        </i>\r\n    </div>\r\n    <article class=\"clips\">\r\n        <button type=\"button\" class = \"play-button\">\r\n            <img src= \"playbutton.png\" height=\"42\" width=\"42\">\r\n        </button>\r\n\r\n        <div class=\"clips-content\">\r\n            <p class=\"clips-text\">\r\n\r\n                API SHOULD BE EXTEND FROM HERE\r\n                \r\n                <p1 class=\"clips-time\">\r\n\r\n                    3:58\r\n\r\n                </p1>\r\n            </p>\r\n        </div>\r\n    </article>\r\n    <div class = \"author-comments\">\r\n\r\n      "
     + alias4(((helper = (helper = helpers.comments || (depth0 != null ? depth0.comments : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"comments","hash":{},"data":data}) : helper)))
-    + "\r\n\r\n    </div>\r\n</main>";
+    + "\r\n\r\n    </div>\r\n</article>";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -1,17 +1,29 @@
+/////////////////////////////////
+//  Setup global dependencies  //
+/////////////////////////////////
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
+///////////////////////////////////////////////////////////////////////
+//  Initiate express and handlebars and set port to desired or 3000  //
+///////////////////////////////////////////////////////////////////////
 var app = express();
 var port = process.env.PORT || 3000;
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+
+////////////////////////////////////////
+//  Render home page as default page  //
+////////////////////////////////////////
 app.get('/', function (req, res, next) {
   res.status(200).render('home');
 });
 
+/////////////////////////////////////////////
+//  Render page of file typed into browser //
+/////////////////////////////////////////////
 app.use(express.static('public'));
 
 app.listen(port, function () {
